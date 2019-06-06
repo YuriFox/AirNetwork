861a979fc6d9e7fc5fc2fdd7ef741da989bac532
Yuri <yuri17fox@gmail.com>
2019-06-06 21:20:55 +0300

    Removed gitignore

---
+++ Source/AirNetwork.h
@@ -0,0 +1,19 @@
+//
+//  AirNetwork.h
+//  AirNetwork
+//
+//  Created by Yuri Fox on 01.08.2018.
+//  Copyright © 2018 Yuri Lysytsia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirNetwork.
+FOUNDATION_EXPORT double AirNetworkVersionNumber;
+
+////! Project version string for AirNetwork.
+//FOUNDATION_EXPORT const unsigned char AirNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirNetwork/PublicHeader.h>
+
+
